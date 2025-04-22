50e79978104677d83554555444a34266e39e39de
akashsiripuram <akashsiripuram05@gmail.com>
2025-04-22 19:04:50 +0530

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose=require("mongoose");
+const router = require("./routes/user.route");
+const cors = require("cors");
+const verifyToken = require("./middleware/verifyToken");
+const app = express();
+
+app.use(express.json())
+
+app.use("/api",router);
+
+
+app.get("/",verifyToken,(req,res)=>{
+  res.send("Hello World")
+})
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+app.listen(5000, () => {
+  console.log("listening to a port of 5000");
+});
---
+++ backend/routes/user.route.js
@@ -0,0 +1,9 @@
+const { createUser, loginUser, getUser } = require("../controllers/auth.controller");
+const verifyToken = require("../middleware/verifyToken");
+
+const router = require("express").Router();
+
+router.post("/signup", createUser);
+router.post("/login",loginUser);
+router.get("/get-user",verifyToken,getUser)
+module.exports = router;
