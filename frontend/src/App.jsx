7222b2d3f802b8d2dd08749521fc0e242b98a8d0
akashsiripuram <akashsiripuram05@gmail.com>
2025-05-25 22:42:47 +0530

    fix:5

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Auth/Login/Login';
-import Register from './pages/Auth/register/register'
+import Register from "./pages/Auth/register/Register";
 import CalendarPage from './pages/Calender/CalenderPage'
 import Dashboard from './pages/Dashboard/Dashboard'
 import Profile from './pages/Profile/Profile'
