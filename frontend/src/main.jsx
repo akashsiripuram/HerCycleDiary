ba7b1c5a6e06058a7447de9063f922fb8b197d51
akashsiripuram <akashsiripuram05@gmail.com>
2025-05-25 22:26:11 +0530

    fix:2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'sonner'
-import { ThemeProvider } from './contexts/ThemeContext.jsx'
+import { ThemeProvider } from './Contexts/ThemeContext.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <StrictMode>
